5810befaa6abd8ad730f62980eee0c86cfe0d17c
Saurav Saha <saurav.saha1984@gmail.com>
2026-08-28 01:47:57 +0530

    Time: 2 ms (76.35%) | Memory: 20 MB (86.93%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,10 +1,14 @@
 class Solution:
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # Start from the row above the bottom row and move upwards
-        for row in range(len(triangle) - 2, -1, -1):
-            for col in range(len(triangle[row])):
-                # Pick the minimum of the two adjacent choices from the row below
-                triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1])
+    def minimumTotal(self, triangle):
         
-        # The top cell now contains the minimum path sum
-        return triangle[0][0]
+        dp = triangle[-1][:]
+
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                
+                dp[j] = triangle[i][j] + min(
+                    dp[j],
+                    dp[j + 1]
+                )
+
+        return dp[0]
